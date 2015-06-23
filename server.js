11a40feb23388c354000b3af16912ee3f94bcbd7
Jennifer Mah <jen.mah@hotmail.com>
2015-06-23 12:18:10 +0100

    working on login and signup pages

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 console.log('Oh hey guuurl!¡!¡!')
 
-
-
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -9,10 +7,16 @@ var path = require('path');
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var mongoose = require('mongoose')
 var db = require('./models')
+
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 app.set('views','./views');
 app.set('view engine', 'ejs');
@@ -22,9 +26,23 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function(req, res){
   res.render('index');
 })
+
+app.get('/login', function(req, res) {
+  res.render('login.ejs'); 
+});
+
+app.get('/signup', function(req, res) {
+  res.render('signup.ejs'); 
+});
 
 app.get('/places', function(req, res){
   db.Location.find({}, function(err, places){
