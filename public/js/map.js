63002ca2bf200522dcd89d6c87e3b92c5c8d5ce8
David James <d.james88@btinternet.com>
2015-06-23 16:59:46 +0100

    conflict resolved

--- public/js/map.js
+++ public/js/map.js
@@ -44,7 +44,6 @@ function codeAddress(locationData) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
           map: map,
-          icon: "./assets/images/tree.png",
           position: results[0].geometry.location
       }); 
       ///passes thru 3 data for ajax request
