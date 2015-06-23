a1bd9e95f7d60dca8c5069a6cb1405f10b91e710
David James <d.james88@btinternet.com>
2015-06-23 10:54:04 +0100

    merge conflicts resolved

---
+++ public/js/event-listeners.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {  
+
+function addTrip() {
+    console.log('addTrip');
+    $("#left-pop-out").toggle();
+    $('#left-pop-out').addClass('animated slideInLeft');
+    // $('#left-pop-out').addClass('animated slideOutLeft');
+}
+
+function login() {
+    console.log('addTrip');
+    $("#login-pop-out").toggle();
+    $('#left-pop-out').addClass('animated slideInLeft');
+}
+
+function contactMenu() {
+    console.log('contactMenu');
+}
+
+$('.fa-plus').on('click', addTrip);
+
+$('.fa-pencil').on('click', contactMenu);
+
+$('#login-pop-out').on('click', login);
+
+})
