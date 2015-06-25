41b236759a9b0bc28ab00c78ec712250bdf90a1b
Jennifer Mah <jen.mah@hotmail.com>
2015-06-25 09:41:55 +0100

    text boxes are working, but refresh is not

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,42 +1,42 @@
 // console.log('this works');
 
-var locations = [];
 
 $(document).ready(function(){
   // console.log('this works');
   $.get('/test', function(places){
     // console.log(places);
     }).done(function(places){
-      console.log(places, "done function");
-      $.each(places, function(index, place){
-        // console.log(place.lat, "our each loop");
-        var city = place.city;
+      console.log(places, "this is places after .done");
+      var locations = [];
+      var infowindow = new google.maps.InfoWindow();
+      var i;
+      $.each(places, function(index, place) {
+        console.log(place, "this is place");
+        var three = '';
+        
+        place.three_things.forEach(function(e,i){
+          console.log('forEach is running');
+         three += e.text
+        });
         var lat = place.lat;
         var lng = place.lng;
-        var locations = [city, lat, lng];
-        three = place.three_things.forEach(function(e,i){
-          console.log(e.text, e.lat, e.lng);
-          var text = e.text
-          console.log(text)
-        });
-
-        getShow(city, lat, lng);
-        function getShow() {
-          var infowindow = new google.maps.InfoWindow();
-
-          var myLatlng = new google.maps.LatLng( lat, lng);
-          var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-           
-          });
-
-       }
-       google.maps.event.addDomListener(window, 'load', getShow);
-
+        var placeInfo = [three, lat, lng];
+        locations.push(placeInfo);
+        console.log(locations, "this is locations");
       })
+      for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
     })
 })
-
-
 
--- seed.js
+++ seed.js
@@ -8,34 +8,35 @@ db.User.collection.remove();
 db.Location.collection.remove();
 db.ThreeThings.collection.remove();
 
-db.User.create({
-  name: 'User One',
-  username: 'UzeeeerOne',
-  password: 'password',
-  email: 'userone@test.com'
-}, function(err, user){
-  console.log('user created');
-  console.log(user);
-  db.Location.create({
-    country: 'England',
-    city: 'London',
-    date_of_visit: '10/10/2010',
-     duration_of_visit: '2 weeks'
-  }, function(err, location){
-    console.log('location created');
-    console.log(location);
-    db.ThreeThings.create({
-      text: 'test1',
-      lat: '2343454',
-      lng: '133435'
-    }, function(err, threeThings){
-      console.log('things created');
-      console.log(threeThings);
-      location.three_things.push(threeThings);
-      user.locations.push(location);
-      user.save();
-    })
-  })
-  })
+// db.User.create({
+//   name: 'User One',
+//   username: 'UzeeeerOne',
+//   password: 'password',
+//   email: 'userone@test.com'
+// }, function(err, user){
+//   console.log('user created');
+//   console.log(user);
+//   db.Location.create({
+//     country: 'England',
+//     city: 'London',
+//     date_of_visit: '10/10/2010',
+//      duration_of_visit: '2 weeks'
+//   }, function(err, location){
+//     console.log('location created');
+//     console.log(location);
+//     db.ThreeThings.create({
+//       text: 'test1',
+//       lat: '2343454',
+//       lng: '133435'
+//     }, function(err, threeThings){
+//       console.log('things created');
+//       console.log(threeThings);
+//       location.three_things.push(threeThings);
+//       user.locations.push(location);
+//       user.save();
+//     })
+//   })
+//   })
+
  // blog.comments.push(comment);
  //     blog.save()
